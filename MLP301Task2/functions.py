184a2855ddecaf34a2a8678a27e1251156e0c968
Duong Dono <duongdono@gmail.com>
2024-11-03 16:40:13 +1300

    task 2

--- MLP301Task2/functions.py
+++ MLP301Task2/functions.py
@@ -5,6 +5,9 @@ from skimage.feature import graycomatrix, graycoprops, hog
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import os
+from sklearn.metrics import (accuracy_score, auc, f1_score, precision_score,
+                             recall_score, roc_auc_score, roc_curve)
+import matplotlib.pyplot as plt
 
 def imagePreprocessing(rgb_image):
   hsv_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2HSV)
@@ -49,7 +52,7 @@ def HOGCalculator(gray_roi):
     features = [round(float(f), 2) for f in features]
     return features
 
-def PCAHOGCalculator(HOG_features_list, n_component= 0.95):
+def PCAHOGCalculator(HOG_features_list, n_component= 50):
   name_list = [item[0] for item in HOG_features_list]
   label_list = [item[1] for item in HOG_features_list]
   features_list = [item[2] for item in HOG_features_list]
@@ -84,3 +87,25 @@ def writeFeaturesToCSV(csv_name, row, header, main_folder, sub_folder):
         writer.writerow([row[0]] + [row[1]] + list(row[2]))
         
         
+def display_metrics(model_name, y_test, y_pred, y_test_binarized, y_score):
+    print(f"Performance Metrics for {model_name}")
+    print(f"Accuracy: {accuracy_score(y_test, y_pred):.2f}")
+    print(f"Precision: {precision_score(y_test, y_pred, average='weighted'):.2f}")
+    print(f"Recall: {recall_score(y_test, y_pred, average='weighted'):.2f}")
+    print(f"F1-Score: {f1_score(y_test, y_pred, average='weighted'):.2f}")
+    auc_score = roc_auc_score(y_test_binarized, y_score, average='weighted', multi_class="ovr")
+    print(f"AUC Score: {auc_score:.2f}\n")
+    
+def plot_roc_curves_multiclass(y_test_binarized, y_scores, model_name, n_classes):
+    plt.figure(figsize=(10, 7))
+    for i in range(n_classes):
+        fpr, tpr, _ = roc_curve(y_test_binarized[:, i], y_scores[:, i])
+        roc_auc = auc(fpr, tpr)
+        plt.plot(fpr, tpr, label=f'{model_name} class {i+1} (AUC = {roc_auc:.2f})')
+    
+    plt.plot([0, 1], [0, 1], 'k--', label="Random Guessing")
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title(f"ROC Curves for {model_name}")
+    plt.legend(loc="lower right")
+    plt.show()
